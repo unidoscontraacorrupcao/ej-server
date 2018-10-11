f6406877ce41c12a92506621198f3616addce3a5
David Carlos <ddavidcarlos1392@gmail.com>
2018-10-11 11:17:03 -0300

    Enable mailgun.

--- src/ej/settings/__init__.py
+++ src/ej/settings/__init__.py
@@ -218,11 +218,11 @@ class Conf(ThemesConf,
                          '18.222.20.172']
 
         DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
-        ACCOUNT_EMAIL_VERIFICATION = 'optional'
-        EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-        #EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
-        #ANYMAIL = {'MAILGUN_API_KEY': ''}
-        #DEFAULT_FROM_EMAIL = "Unidos Contra a Corrupção <noreply@unidoscontraacorrupcao.org.br>"
+        #ACCOUNT_EMAIL_VERIFICATION = 'optional'
+        #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+        EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
+        ANYMAIL = {'MAILGUN_API_KEY': ''}
+        DEFAULT_FROM_EMAIL = "Unidos Contra a Corrupção <noreply@unidoscontraacorrupcao.org.br>"
 
 Conf.save_settings(globals())
 #
